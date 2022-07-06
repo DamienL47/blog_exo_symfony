cc76219404c47f0b727cfb812fe84662f0ef252d
DamienL47 <damien.lataste@lapiscine.pro>
2022-07-06 11:32:34 +0200

    ajout de colonnes image isPublished et author dans bdd existante

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,20 +5,20 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
-//Je créé un Objet Relationnel de Mapping (ORM) gr^ce au framework Doctrine utilisé par symfony
+//Je créé une entité avec des annotations qui sera créé en table gr^ce au framework Doctrine utilisé par symfony "Objet Relationnel de Mapping" (ORM)
 /**
  * @ORM\Entity()
  */
 // je créé une classe pour définir le nom de la table en BDD
 class Article
 {
-    //j'instancie ma classe en lui passant les valeurs des colonnes
+    //je fait une propriété et je la mappe en lui passant les valeurs des colonnes
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
-    // Je lui passe ensuite le titre de la colonne en méthode 
+    // Je lui passe ensuite le titre de la colonne en méthode
     public $id;
 
     /**
@@ -26,4 +26,18 @@ class Article
      */
     public $title;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    public $image;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    public $isPublished;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    public $author;
 }
